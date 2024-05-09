80387be0992878fdb114c753a2acb6db07dd4508
Thomas Gooch <Thomas.Gooch@McKesson.com>
2024-05-09 15:21:52 -0400

    initial commit basic project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesListComponent } from './Recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './Recipes/recipes-detail/recipes-detail.component';
+import { RecipeItemComponent } from './Recipes/Recipes-List/recipe-item/recipe-item.component';
+import { ShoppingEditComponent } from './Shopping-List/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppingListComponent,
+    RecipesComponent,
+    HeaderComponent,
+    RecipesListComponent,
+    RecipesDetailComponent,
+    RecipeItemComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
